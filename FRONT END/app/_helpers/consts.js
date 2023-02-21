c7ac94bbc6284228d7bbd14a2f1817d102d1d18d
ajaybhattnagar <ajay.bhattnagar21@gmail.com>
2023-02-20 23:17:28 -0500

    commit

--- FRONT END/app/_helpers/consts.js
+++ FRONT END/app/_helpers/consts.js
@@ -1,7 +1,7 @@
 export const appConstants = {
     SEARCH_DELAY_MS: 500,
-    // BASE_URL: 'http://3.138.106.64:5000/',
-    BASE_URL: 'http://localhost:5000/',
+    BASE_URL: 'http://3.138.106.64:5000/',
+    // BASE_URL: 'http://localhost:5000/',
 
     REGISTER: 'api/v1/register',
     LOGIN: 'api/v1/login',
