c128654ae269a40cfc269a93abcce72d20cace99
ajaybhattnagar <ajay.bhattnagar21@gmail.com>
2024-02-07 22:48:07 -0500

    commit

--- FRONT END/app/_helpers/consts.js
+++ FRONT END/app/_helpers/consts.js
@@ -1,12 +1,12 @@
 export const appConstants = {
     SEARCH_DELAY_MS: 500,
 
-    DEPLOYEMENT_URL: 'http://localhost:8080/',
-    // DEPLOYEMENT_URL: 'https://uni-vm-visdev.unified.local/',
+    // DEPLOYEMENT_URL: 'http://localhost:8080/',
+    DEPLOYEMENT_URL: 'https://uni-vm-visdev.unified.local/',
     // DEPLOYEMENT_URL: 'http://uni-vm-visdev.unified.local:5003/',
 
-    BASE_URL: 'http://localhost:5000/',
-    // BASE_URL: 'https://uni-vm-visdev.unified.local:5002/',
+    // BASE_URL: 'http://localhost:5000/',
+    BASE_URL: 'https://uni-vm-visdev.unified.local:5002/',
     // BASE_URL: 'http://uni-vm-visdev.unified.local:5001/',
     
 
